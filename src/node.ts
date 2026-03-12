8cf2837aa6dcd5ae52e3b97f2a3d37d1ef30cbf9
Carlos Faustino <chfcchfc96@gmail.com>
2026-03-12 09:45:46 -0300

    fix: reorganize deps between `dependencies` and `devDependencies`

--- src/node.ts
+++ src/node.ts
@@ -1,67 +1,74 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import unusedImports from "eslint-plugin-unused-imports";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import { defineConfig, globalIgnores } from "eslint/config"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import unusedImports from "eslint-plugin-unused-imports"
+import globals from "globals"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
-});
+})
 
-export default defineConfig([{
-  languageOptions: {
-    globals: {
-      ...globals.node,
+export default defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {},
     },
 
-    parser: tsParser,
-    ecmaVersion: "latest",
-    sourceType: "module",
-    parserOptions: {},
-  },
+    extends: compat.extends(
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+    ),
 
-  extends: compat.extends(
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ),
+    plugins: {
+      // @ts-ignore
+      "@typescript-eslint": typescriptEslint,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
 
-  plugins: {
-    // @ts-ignore
-    "@typescript-eslint": typescriptEslint,
-    "simple-import-sort": simpleImportSort,
-    "unused-imports": unusedImports,
-  },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "avoid",
+          endOfLine: "auto",
+          printWidth: 80,
+          semi: false,
+          singleAttributePerLine: true,
+          tabWidth: 2,
+        },
+      ],
 
-  rules: {
-    "prettier/prettier": ["warn", {
-      arrowParens: "avoid",
-      endOfLine: "auto",
-      printWidth: 80,
-      semi: false,
-      singleAttributePerLine: true,
-      tabWidth: 2,
-    }],
+      "no-unused-vars": "off",
+      "no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "simple-import-sort/imports": "warn",
+    },
 
-    "no-unused-vars": "off",
-    "no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "unused-imports/no-unused-imports": "warn",
-    "simple-import-sort/imports": "warn",
-  },
-
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
+      },
     },
   },
-}]);
+  globalIgnores(["**/node_modules"])
+])
